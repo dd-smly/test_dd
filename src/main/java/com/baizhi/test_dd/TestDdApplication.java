ac33479a07a551c15f1c09a44ce8cfc3761cd32d
dd <1834839522@qq.com>
2019-09-25 16:21:08 +0800

    第一次创建项目

---
+++ src/main/java/com/baizhi/test_dd/TestDdApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.test_dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baizhi/test_dd/TestDdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baizhi.test_dd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestDdApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
